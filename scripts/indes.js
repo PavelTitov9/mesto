03c60f1b906e3a964b51fb90dfb7f385b2db25f1
Pavel Titov <tuturusenpui@yandex.ru>
2023-08-07 12:11:35 +0300

    feature/feature-name

---
+++ scripts/indes.js
@@ -0,0 +1,34 @@
+let buttonform =document.querySelector('.profile__open');
+let formopen =document.querySelector('.profile__btn');
+let popup = document.querySelector('.popup');
+let close =document.querySelector('.popup__close');
+let formElement =document.querySelector('.popup__form');
+let nameInput =document.querySelector('.popup__input_type_name');
+let jobInput =document.querySelector('.popup__input_type_about');
+let name =document.querySelector('.profile__name');
+let job =document.querySelector('.profile__bio');
+
+function toggle(){
+    if (popup.style.display ==='none'|| popup.style.display===''){
+        popup.style.display='flex';
+        nameInput.value=name.textContent;
+        jobInput.value=job.textContent;
+    }else{
+        popup.style.display='none';
+    }
+    
+}
+
+buttonform.addEventListener('click',toggle);
+close.addEventListener('click',toggle);
+formopen.addEventListener('click',toggle);
+
+formElement.addEventListener('submit', handleFormSubmit);
+function handleFormSubmit (evt) {
+    evt.preventDefault(); 
+    let textName= nameInput.value;
+    let textJob= jobInput.value;
+    name.textContent=textName;
+    job.textContent=textJob;
+    toggle();
+}
